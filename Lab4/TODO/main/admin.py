fcd7758e5fc44885bdf51aa742250e6e0d8eb7a6
Abilay Aytbanov <aitbanov640@gmail.com>
2022-03-12 04:09:28 +0600

    Lab4

---
+++ Lab4/TODO/main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import TodoList, Todo
+
+@admin.register(Todo)
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ('task', 'created', 'due_on', 'owner', 'mark',)
+    search_fields = ('task', 'mark',)
+    list_filter = ('task', 'created', 'due_on', 'owner', 'mark',)
+
+@admin.register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+    list_filter = ('title',)
