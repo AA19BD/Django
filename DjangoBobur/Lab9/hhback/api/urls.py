ec20158c2fe41877280eb48f2ea168649ddf9cc2
Abilay Aytbanov <aitbanov640@gmail.com>
2022-04-05 01:52:43 +0600

    JWT-IsAuthenticated-obtain_jwt_token

--- DjangoBobur/Lab9/hhback/api/urls.py
+++ DjangoBobur/Lab9/hhback/api/urls.py
@@ -1,14 +1,14 @@
-from unicodedata import name
-from django.contrib import admin
 from django.urls import path
 #from .views import list_companies,company_detail,list_vacancies,vacancy_detail,list_vacancies_by_company,top_ten_vacancies
 from .views import CompaniesListAPIView,CompanyDetailAPIView,VacancyListAPIView,VacancyDetailAPIView
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('companies/',CompaniesListAPIView.as_view(),name='list_companies'),
     path('companies/<int:pk>',CompanyDetailAPIView.as_view(),name='company_detail'),
-    # path('companies/<int:pk>/vacancies',list_vacancies_by_company,name='list_vacancies_by_company'),
     path('vacancies/',VacancyListAPIView.as_view(),name='list_vacancies'),
-    path('vacancies/<int:pk>',VacancyDetailAPIView.as_view(),name='vacancy_detail')
-    # path('vacancies/top_ten',top_ten_vacancies,name='top_ten_vacancies')
+    path('vacancies/<int:pk>',VacancyDetailAPIView.as_view(),name='vacancy_detail'),
+    path('login/',obtain_jwt_token)
+    # path('vacancies/top_ten',top_ten_vacancies,name='top_ten_vacancies'),
+# path('companies/<int:pk>/vacancies',list_vacancies_by_company,name='list_vacancies_by_company'),
 ]
--- DjangoBobur/Lab9/hhback/api/views/views_generic_v2.py
+++ DjangoBobur/Lab9/hhback/api/views/views_generic_v2.py
@@ -1,20 +1,25 @@
 from rest_framework import generics
 from ..models import Company,Vacancy
 from ..serializers import CompanySerializer2,VacancySerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CompaniesListAPIView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer2
+    permission_classes=(IsAuthenticated,)
 
 class CompanyDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer2
+    permission_classes = (IsAuthenticated,)
 
 
 class VacancyListAPIView(generics.ListCreateAPIView):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
+    permission_classes = (IsAuthenticated,)
 
 class VacancyDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
+    permission_classes = (IsAuthenticated,)
