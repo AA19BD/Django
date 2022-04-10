f4e9c9a9ba8c12edccdea1af11a26574b16b844c
Abilay Aytbanov <aitbanov640@gmail.com>
2022-04-10 23:41:09 +0600

    DRf-ModelSerializer-ViewSets-Routers-Paginations-Validations

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -1,22 +1,26 @@
 import json
-from django.forms.models import model_to_dict
-from django.http import JsonResponse
-from  products.models import Product
 import sys
 import os
 sys.path.append(os.getcwd())
-# Create your views here.
+from django.forms.models import model_to_dict
+# from django.http import JsonResponse
+from rest_framework.response import  Response
+from rest_framework.decorators import api_view
+from  products.models import Product
+from  products.serializers import ProductSerializer
 
+@api_view(["GET","POST"])
 def api_home(request,*args,**kwargs):
-    model_data=Product.objects.all().order_by('?').first()
+    instance=Product.objects.all().order_by('?').first()
     # data={}
-    if model_data:
-        data=model_to_dict(model_data,fields=['title','content','price'])
+    if instance:
+        # data=model_to_dict(model_data,fields=['title','content','price','sale_price'])
+        data=ProductSerializer(instance).data
         # data['id']=model_data.id
         # data["title"]=model_data.title
         # data['content']=model_data.content
         # data['price']=model_data.price
-    return JsonResponse(data)
+    return Response(data)
 
     # try:
     #     data=json.loads(request.body) #String of JSON data- >Python dict
